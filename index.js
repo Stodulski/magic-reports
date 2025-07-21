a906e9f4294d389ca46bf56c6676b7260051909d
Maximo Stodulski <74630678+Stodulski@users.noreply.github.com>
2025-07-21 17:15:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/api/ejecutar-reporte', async (req, res) => {
   try {
-    await generarYEnviarReporteExcel();
     res.status(200).send('✅ Reporte enviado correctamente por email');
+    await generarYEnviarReporteExcel();
   } catch (error) {
     res.status(500).send('❌ Error al enviar el reporte: ' + error.message);
   }
